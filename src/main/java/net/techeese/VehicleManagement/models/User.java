54e08bda5f758607dd5b62f094db7e540cae909a
Thai Tran <thai@thaitech.net>
2022-09-29 20:53:48 +0700

    Add models

---
+++ src/main/java/net/techeese/VehicleManagement/models/User.java
@@ -0,0 +1,36 @@
+package net.techeese.VehicleManagement.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User {
+		
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private int id;
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+}
